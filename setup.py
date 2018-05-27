8b15c1071c34a96ab0e5772867752d9fb45c3ff0
Jan Pajdak <pajdakjan@gmail.com>
2018-05-27 20:12:06 +0200

    Ver 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dpp_calendar_lib',
-      version='0.1',
+      version='0.3',
       author='Jan Pajdak',
       author_email='pajdakjan@gmail.com',
       url='https://github.com/janjpk/S6-DPP-10B',
